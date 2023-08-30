1a0fd5484180dd1a1655895169660577c58e3d72
marcos-git01 <marcoslefama02@gmail.com>
2023-08-30 05:48:53 -0300

    Commit 6: ClienteControlador completo y creacion cliente_list y cliente_modificar

--- EjExtraSpring-estancias/estancias/src/main/java/com/estancias/controladores/ClienteControlador.java
+++ EjExtraSpring-estancias/estancias/src/main/java/com/estancias/controladores/ClienteControlador.java
@@ -1,5 +1,6 @@
 package com.estancias.controladores;
 
+import com.estancias.entidades.Cliente;
 import com.estancias.entidades.Usuario;
 import com.estancias.excepciones.MiException;
 import com.estancias.servicios.ClienteServicio;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,5 +64,70 @@ public class ClienteControlador {
 
         return "cliente_form.html";
     }
+    
+    //@PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @GetMapping("/lista") //localhost:8080/cliente/lista
+    public String listar(ModelMap modelo) {
+
+        List<Cliente> clientes = clienteServicio.listarClientes();
+
+        modelo.addAttribute("clientes", clientes);
+
+        return "cliente_list.html";
+
+    }
+    
+    @GetMapping("/modificar/{id}") //localhost:8080/cliente/modificar/{id}
+    public String modificar(@PathVariable String id, ModelMap modelo) {
+
+        modelo.put("cliente", clienteServicio.getOne(id));
+
+        List<Usuario> usuarios = usuarioServicio.listarUsuarios();
+
+        modelo.addAttribute("usuarios", usuarios);
+
+        return "cliente_modificar.html";
+    }
+    
+    @PostMapping("/modificar/{id}")
+    public String modificar(@PathVariable String id, String nombre, String calle, Integer numero, String codPostal, String ciudad, String pais, String email, String idUsuario, ModelMap modelo) {
+
+        try {
+
+            clienteServicio.actualizar(id, nombre, calle, numero, codPostal, ciudad, pais, email, idUsuario);
+
+            //Ver esta linea si funciona?
+            modelo.put("exito", "El Cliente fue modificado correctamente!");
+
+            return "redirect:../lista";
+
+        } catch (MiException ex) {
+
+            modelo.put("error", ex.getMessage());
+
+            return "cliente_modificar.html";
+        }
+
+    }
+    
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable String id, ModelMap modelo) throws MiException {
+   
+        try {
+
+            clienteServicio.eliminarCliente(id);
+
+            return "redirect:../lista";
+
+        } catch (MiException ex) {
+
+            modelo.put("error", ex.getMessage());
+
+            //return "noticia_eliminar.html";
+            return "redirect:../lista";
+        }
+
+    }
+    
 
 }
